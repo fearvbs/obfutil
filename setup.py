8e551c0ad87d56d42e67b0e4b1fc39770947bf19
fearvbs <arab.baraban@gmail.com>
2025-11-23 22:51:25 +0300

    Update setup.py
    Correct version of setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="obfutil",
-    version="3.0.0",
+    version="3.1",
     packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "obfutil=obfutil.ui.cli.main:main",
-        ],
-    },
     install_requires=[
         "cryptography>=3.4",
         "astor>=0.8",
+        "ast",
     ],
-    python_requires=">=3.7",
+    entry_points={
+        'console_scripts': [
+            'obfutil=obfutil.ui.cli.main:main',
+        ],
+    },
+    python_requires=">=3.9",
 )
